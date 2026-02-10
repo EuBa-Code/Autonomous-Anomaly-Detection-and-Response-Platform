173469e46edf375dcb0498f42cb8e271219899ee
Daniele <delitedaniele@gmail.com>
2026-02-10 15:33:37 +0100

    Feature store apply, compose and tree structure

--- services/feature_store_src/feature_store/src/feature_services.py
+++ services/feature_store_src/feature_store/src/feature_services.py
@@ -1,11 +1,10 @@
 from feast import FeatureService 
-from features import machine_batch_view, machine_stream_view
+from features import machine_view
 
 # Create a feature service for the machine view 
 machine_feature_service_v1 = FeatureService(
-    name="machine_feature_service_v1",
+    name="machine_anomaly_service_v1",
     features=[
-        machine_stream_view,
-        machine_batch_view
+        machine_view,
     ],
 )
--- services/feature_store_src/feature_store/src/features.py
+++ services/feature_store_src/feature_store/src/features.py
@@ -7,7 +7,7 @@ from src.data_sources import stream_source, batch_source
 
 #  Create the Feature View
 #  A Feature View groups related features and defines how they are stored and served
-machine_stream_view = FeatureView(
+machine_view = FeatureView(
     name="machine_stream_features",
     entities=[machine],
     ttl=timedelta(hours=24),  # Time-to-live for the features in the online store (Redis)
@@ -28,24 +28,3 @@ machine_stream_view = FeatureView(
     online=True  # Enables serving these features from the online store (e.g. Redis)
 )
 
-
-machine_batch_view = FeatureView(
-    name="machine_batch_features",
-    entities=[machine],
-    ttl=timedelta(365),
-    schema=[
-        Field(name="Current_avg", dtype=Float32),
-        Field(name="Apparent_Power", dtype=Float32),
-        Field(name="Active_Power", dtype=Float32),
-        Field(name="Reactive_Power", dtype=Float32),
-        Field(name="Power_Factor", dtype=Float32),
-        Field(name="THD_Current", dtype=Float32),
-        Field(name="Current_P_to_P", dtype=Float32),
-        Field(name="Max_Current_Instance", dtype=Float32),
-        Field(name="Inrush_Peak", dtype=Float32),
-        Field(name="Phase_imbalance", dtype=Float32),
-        Field(name="Energy_per_Cycle_Wh", dtype=Float32),
-    ],
-    source=batch_source,
-    online=False
-)
