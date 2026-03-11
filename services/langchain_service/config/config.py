76575d26bc771fa81f246a8b3bffd09d38f7b146
DanielePioGenovese <delitedaniele@gmail.com>
2026-03-11 17:23:54 +0100

    Ingestion RAG & MakeFle
    Changed PATH variable in Dockerfile for the ingestion RAG, not it works correctly with the CUDA version and changed the Makefile to work correctly for the block_2 (Added Redpanda)

--- services/langchain_service/config/config.py
+++ services/langchain_service/config/config.py
@@ -5,7 +5,7 @@ from typing import Final
 class Config(BaseSettings):
     mcp_server_uri: str = 'http://mcp_server:8020/mcp'
     vllm_base_url: str = 'http://vllm:8000/v1'
-    chat_model: str = 'Qwen/Qwen3-8B-Instruct'
+    chat_model: str = 'Qwen/Qwen2.5-7B-Instruct'
     slack_webhook_url: str | None = None  
 
 
